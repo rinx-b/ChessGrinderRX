2d467edd65729d96159bc1c02058406054b99d5e
rinx-b <109336579+rinx-b@users.noreply.github.com>
2025-01-10 09:37:11 +0100

    Update loc.ts to include locale_ukr.ts

--- frontend/src/strings/loc.ts
+++ frontend/src/strings/loc.ts
@@ -1,9 +1,11 @@
 import locale_default from "./locale_default";
+import locale_ukr from "./locale_ukr"; 
 import locale_ru from "./locale_ru";
 import {useContext} from "react";
 import {LanguageContext} from "contexts/LanguageContext";
 
 let locales: { [lang: string]: { [word: string]: string } } = {
+    "ukr": locale_ukr,
     "ru": locale_ru,
     "en": locale_default,
     "default": locale_default,
